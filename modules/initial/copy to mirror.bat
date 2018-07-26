f6828347e735e26925de1ea82bc48e7c8014e06a
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-07-26 18:56:27 +1000

    update

---
+++ modules/initial/copy to mirror.bat
@@ -0,0 +1,6 @@
+set S=%USERPROFILE%\Documents\GitHub\wappsystem\isleephealth\modules\initial
+set D=%USERPROFILE%\Documents\GitHub\wappsystem\isleephealth-patients-portal\modules\initial\mirror
+
+xcopy %S%\*.* %D%\*.* /D /Y
+
+pause
